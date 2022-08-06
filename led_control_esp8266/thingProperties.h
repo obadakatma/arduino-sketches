d70725abe0db22927d597f95d608aa6a13551966
obada katma <obadaktm@gmail.com>
2022-08-06 06:33:46 +0300

    added led_control_esp8266

--- led_control_esp8266/thingProperties.h
+++ led_control_esp8266/thingProperties.h
@@ -3,22 +3,22 @@
 #include <ArduinoIoTCloud.h>
 #include <Arduino_ConnectionHandler.h>
 
-const char DEVICE_LOGIN_NAME[]  = "c36f53a1-eea8-45ed-a5ab-fa1227133607";
+const char DEVICE_LOGIN_NAME[] = "c36f53a1-eea8-45ed-a5ab-fa1227133607";
 
-const char SSID[]               = "DR.Moaz";    // Network SSID (name)
-const char PASS[]               = "bigBoss.1962";    // Network password (use for WPA, or use as key for WEP)
-const char DEVICE_KEY[]  = "UOIMJVSSZUALSQTPWN7D";    // Secret device password
+const char SSID[] = ""; // Network SSID (name)
+const char PASS[] = ""; // Network password (use for WPA, or use as key for WEP)
+const char DEVICE_KEY[] = ""; // Secret device password
 
 void onLedChange();
 
 CloudLight led;
 
-void initProperties(){
+void initProperties()
+{
 
-  ArduinoCloud.setBoardId(DEVICE_LOGIN_NAME);
-  ArduinoCloud.setSecretDeviceKey(DEVICE_KEY);
-  ArduinoCloud.addProperty(led, READWRITE, ON_CHANGE, onLedChange);
-
+    ArduinoCloud.setBoardId(DEVICE_LOGIN_NAME);
+    ArduinoCloud.setSecretDeviceKey(DEVICE_KEY);
+    ArduinoCloud.addProperty(led, READWRITE, ON_CHANGE, onLedChange);
 }
 
 WiFiConnectionHandler ArduinoIoTPreferredConnection(SSID, PASS);
